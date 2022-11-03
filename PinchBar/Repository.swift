be1726eff5e580425b7c0864dcb15a8ce6b3923d
Lutz Häußler <lutz.haeussler@web.de>
2022-11-03 01:01:55 +0100

    better UX when update available

--- PinchBar/Repository.swift
+++ PinchBar/Repository.swift
@@ -10,66 +10,59 @@ class Repository {
     func checkForUpdates(verbose: Bool) {
         let url = "https://api.github.com/repos/pnoqable/PinchBar/releases/latest"
         URLSession(configuration: .ephemeral).dataTask(with: URL(string: url)!) { data, _, error in
-            if let data = data, let json = try? JSONSerialization.jsonObject(with: data),
-               let dict = json as? NSDictionary, let newVersion = dict["tag_name"] as? String {
-                DispatchQueue.main.async() {
-                    let updateAvailable = newVersion != self.version
-                    let knownVersion = UserDefaults.standard.string(forKey: "knownVersion")
-                    let knownUpdate = newVersion == knownVersion
-                    if updateAvailable && (verbose || !knownUpdate) {
-                        self.updateAvailable(newVersion: newVersion, known: knownUpdate)
-                    } else if verbose {
-                        self.upToDate()
-                    }
-                }
-            } else if verbose {
-                DispatchQueue.main.async() {
-                    let alert = NSAlert()
-                    alert.icon.isTemplate = true
-                    alert.alertStyle = .warning
-                    alert.messageText = error?.localizedDescription ?? "Communication error."
-                    alert.addButton(withTitle: "OK")
-                    
-                    NSApplication.shared.activate(ignoringOtherApps: true)
-                    
-                    alert.runModal()
-                }
-            }
+            self.checkUpdate(data: data, error: error, verbose: verbose)
         }.resume()
     }
     
-    func updateAvailable(newVersion: String, known: Bool) {
-        let alert = NSAlert()
-        alert.icon.isTemplate = true
-        alert.messageText = "PinchBar \(newVersion) is available!"
-        alert.alertStyle = .informational
-        alert.addButton(withTitle: "Update")
-        alert.addButton(withTitle: "OK")
-        alert.showsSuppressionButton = true
-        alert.suppressionButton?.state = known ? .on : .off
-        
-        NSApplication.shared.activate(ignoringOtherApps: true)
-        
-        if alert.runModal() == .alertFirstButtonReturn {
-            openGitHub()
+    private func checkUpdate(data: Data?, error: Error?, verbose: Bool) {
+        guard let data, let json = try? JSONSerialization.jsonObject(with: data) as? [String:Any],
+              let version = json["tag_name"] as? String, let urlString = json["html_url"] as? String,
+              let url = URL(string: urlString) else {
+            return verbose ? asyncAlert("Communication error", error?.localizedDescription) : ()
         }
         
-        if alert.suppressionButton?.state == .on {
-            UserDefaults.standard.set(newVersion, forKey: "knownVersion")
-        } else {
-            UserDefaults.standard.removeObject(forKey: "knownVersion")
+        guard self.version.compare(version, options: .numeric) == .orderedAscending else {
+            return verbose ? asyncAlert("PinchBar is up-to-date!", "Current Version: \(version)") : ()
+        }
+        
+        let updateKnown = version == UserDefaults.standard.string(forKey: "knownVersion")
+        
+        guard verbose || !updateKnown else { return }
+        
+        asyncAlert("PinchBar \(version) is now available!", "Current Version: \(version)") { alert in
+            alert.addButton(withTitle: "View on GitHub")
+            alert.addButton(withTitle: "Ignore for now")
+            alert.showsSuppressionButton = true
+            alert.suppressionButton?.state = updateKnown ? .on : .off
+            
+            if alert.runModal() == .alertFirstButtonReturn {
+                NSWorkspace.shared.open(url)
+            }
+            
+            if alert.suppressionButton?.state == .on {
+                UserDefaults.standard.set(version, forKey: "knownVersion")
+            } else {
+                UserDefaults.standard.removeObject(forKey: "knownVersion")
+            }
         }
     }
     
-    func upToDate() {
-        let alert = NSAlert()
-        alert.icon.isTemplate = true
-        alert.messageText = "PinchBar is up-to-date!"
-        alert.alertStyle = .informational
+    private static func addOkAndRun(alert: NSAlert) {
         alert.addButton(withTitle: "OK")
-        
-        NSApplication.shared.activate(ignoringOtherApps: true)
-        
         alert.runModal()
+    }
+    
+    private func asyncAlert(_ messageText: String, _ informativeText: String?,
+                            _ addButtonsAndRun: @escaping ((NSAlert)->()) = addOkAndRun) {
+        DispatchQueue.main.async {
+            NSApplication.shared.activate(ignoringOtherApps: true)
+            
+            let alert = NSAlert()
+            alert.icon.isTemplate = true
+            alert.messageText = messageText
+            alert.informativeText = informativeText ?? ""
+            
+            addButtonsAndRun(alert)
+        }
     }
 }
